fb0f3b6d9dea9454db2a987521e09f83596550c8
catarak <ctarakajian@gmail.com>
2015-01-19 14:55:33 -0500

    add labels

--- app/assets/javascripts/api/artists.js
+++ app/assets/javascripts/api/artists.js
@@ -8,31 +8,55 @@ $(function() {//get data
 });
 
 //Width and height
-var w = 960;
+var w = 1000;
 var h = 500;
+var padding = 25;
 var barPadding = 1;
 
 function drawChart(data) {
   var y = d3.scale.linear();
-  y.range([h, 0]);
+  y.range([h - padding, padding]);
   y.domain([0, 10.0]);
-  var barWidth = w / data.length;
+  var barWidth = (w - padding*2)/ data.length;
   var chart = d3.select(".bar")
               .attr("width", w)
               .attr("height", h);
   var bar = chart.selectAll("g")
                  .data(data)
                  .enter().append("g")
-                 .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+                 .attr("transform", function(d, i) { return "translate(" + (padding + (i * barWidth)) + ",0)"; });
   bar.append("rect")
      .attr("y", function(d) { return y(d.score); })
-     .attr("height", function(d) { return h - y(d.score); })
+     .attr("height", function(d) { return h - padding - y(d.score); })
      .attr("width", barWidth - barWidth/2);
-  
-}
 
-function drawAxes() {
-  
+  bar.append("text")
+      .attr("x", barWidth / 4)
+      .attr("y", function(d) { return y(d.score) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.score; });
+
+  bar.append("text")
+      .attr("class", "label")
+      .attr("x", barWidth / 4)
+      .attr("y", function(d) { return h - padding + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.release_year; });
+  bar.append("text")
+      .attr("class", "label")
+      .attr("x", barWidth / 4)
+      .attr("y", function(d) { return h - padding + 13; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.album; });
+
+  //draw axis
+  var yAxis = d3.svg.axis();
+  yAxis.scale(y);
+  yAxis.orient("left");
+  chart.append("g")
+       .attr("class", "axis")
+       .attr("transform", "translate(" + padding + ",0)")
+       .call(yAxis);
 }
 
 //just for reference
--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class Api::ArtistsController < ApplicationController
   def index
-    response = open("https://api.morph.io/catarak/pitchfork_review_data/data.json?key=#{ENV['morphio_api_key']}&query=select%20*%20from%20'data'%20where%20artist%20%3D%20%22Bj%C3%B6rk%22").read
+    response = open("https://api.morph.io/catarak/pitchfork_review_data/data.json?key=#{ENV['morphio_api_key']}&query=select%20*%20from%20%27data%27%20where%20artist%20%3D%20%22Bj%C3%B6rk%22%20order%20by%20publish_date%20asc").read
     songs = JSON.parse(response)
     render json: songs
   end
